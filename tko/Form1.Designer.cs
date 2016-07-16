9ae8c57af8c5918e4cc098fc9ac36430630c4af9
Dmitrij Rusakov <Daemon2017@gmail.com>
2016-07-16 13:10:48 +0300

    Программа позволяет обучать конечный автомат

---
+++ tko/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace tko
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 87);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(132, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Текущая температура: 0";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 103);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(142, 34);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Обучить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 48);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(133, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Желаемая температура:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(123, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Текущая температура:";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(12, 25);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            35,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            11,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(142, 20);
+            this.numericUpDown1.TabIndex = 13;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            25,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(12, 64);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            35,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            11,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(142, 20);
+            this.numericUpDown2.TabIndex = 14;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            27,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(160, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 13);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Значения НЛ: ";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(160, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(480, 100);
+            this.pictureBox1.TabIndex = 17;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 143);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(142, 34);
+            this.button2.TabIndex = 18;
+            this.button2.Text = "Отрисовать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(642, 183);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Name = "Form1";
+            this.Text = "Теория КА";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ tko/Form1.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace tko
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        float neededTemperature,
+              currentTemperature;
+
+        float probableTemperature,
+              previousProbableTemperature;
+
+        int bestState;
+
+        int[] usedState = { 0 };
+
+        float CmN1,//Второе слева                               
+              CmN2;//Первое слева
+
+        float NmC1,//Второе справа                   
+              NmC2;//Первое справа
+
+        //Вектор возможных состояний
+        int[] possibleState = { -2, -1, 0, 1, 2 };
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Graphics g = pictureBox1.CreateGraphics();
+
+            g.Clear(Color.White);
+
+            Pen net = new Pen(Brushes.Black, 1);
+
+            for (int i = 10; i < 480; i += 10)
+            {
+                g.DrawLine(net, new Point(i, 0), new Point(i, 100));
+            }
+
+            Pen figure = new Pen(Brushes.Red, 2);
+
+            //Отрисовываем термы слева
+            g.DrawLine(figure, new Point((int)(CmN2 * 10 + 240), 100), new Point((int)((CmN2 + CmN1) / 2 * 10 + 240), 0));
+            g.DrawLine(figure, new Point((int)((CmN2 + CmN1) / 2 * 10 + 240), 0), new Point((int)(CmN1 * 10 + 240), 100));
+
+            g.DrawLine(figure, new Point((int)(CmN1 * 10 + 240), 100), new Point((int)(CmN1 / 2 * 10 + 240), 0));
+            g.DrawLine(figure, new Point((int)(CmN1 / 2 * 10 + 240), 0), new Point(240, 100));
+
+            //Отрисовываем терм нуля
+            g.DrawLine(figure, new Point(240, 0), new Point(240, 100));
+
+            //Отрисовываем термы справа
+            g.DrawLine(figure, new Point(240, 100), new Point((int)(NmC1 / 2 * 10 + 240), 0));
+            g.DrawLine(figure, new Point((int)(NmC1 / 2 * 10 + 240), 0), new Point((int)(NmC1 * 10 + 240), 100));
+
+            g.DrawLine(figure, new Point((int)(NmC1 * 10 + 240), 100), new Point((int)((NmC1 + NmC2) / 2 * 10 + 240), 0));
+            g.DrawLine(figure, new Point((int)((NmC1 + NmC2) / 2 * 10 + 240), 0), new Point((int)(NmC2 * 10 + 240), 100));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int startTemperature;
+
+            //Считываем входные данные
+            startTemperature = Convert.ToInt32(numericUpDown1.Value);
+            neededTemperature = Convert.ToInt32(numericUpDown2.Value);
+
+            float differenceBetween = neededTemperature - startTemperature;
+
+            currentTemperature = startTemperature;
+
+            bool allChecked = false;
+
+            for (int i = 0; i < possibleState.Length; i++)
+            {
+                //Если все возможные значения перебраны - разрешаем сохранение наилучшего варианта
+                if (i == possibleState.Length - 1)
+                {
+                    allChecked = true;
+                }
+
+                //Если мы только начали работу - альтернативой возможному варианту считаем начальное значение
+                if (i == 0)
+                {
+                    previousProbableTemperature = startTemperature;
+                }
+
+                probableTemperature = states(possibleState[i], currentTemperature);
+                previousProbableTemperature = quality(probableTemperature, previousProbableTemperature, possibleState[i], allChecked);
+            }
+
+            learningAlgorithm(differenceBetween, bestState);
+
+            label4.Text = "Текущая температура: " + currentTemperature.ToString();
+            label2.Text = "Значения НЛ: " + CmN2.ToString() + " " + CmN1.ToString() + " " + "0" + " " + NmC1.ToString() + " " + NmC2.ToString();
+        }
+
+        //Список возможных состояний
+        float states(int stateNumber,
+                     float probableTemperature)
+        {
+            switch (stateNumber)
+            {
+                case -2:
+                    probableTemperature--;
+                    probableTemperature--;
+                    break;
+                case -1:
+                    probableTemperature--;
+                    break;
+                case 0:
+                    break;
+                case 1:
+                    probableTemperature++;
+                    break;
+                case 2:
+                    probableTemperature++;
+                    probableTemperature++;
+                    break;
+            }
+
+            return probableTemperature;
+        }
+
+        //Оцениваем качество произведенных изменений
+        float quality(float probableTemperature,
+                      float previousProbableTemperature,
+                      int probableState,
+                      bool allChecked)
+        {
+            //Если предлагаемое изменение УМЕНЬШИЛО разницу между нынешним значением и требуемым
+            //а также если оно лучше, чем прошлые предлагаемые изменения
+            if ((Math.Abs(neededTemperature - probableTemperature) < Math.Abs(neededTemperature - currentTemperature)) &&
+                (Math.Abs(neededTemperature - probableTemperature) < Math.Abs(neededTemperature - previousProbableTemperature)))
+            {
+                //то всё хорошо, запоминаем и перебираем дальше: вдруг есть значения еще лучше?
+                previousProbableTemperature = probableTemperature;
+                bestState = probableState;
+            }
+
+            //Если все значения перебраны - сохраняем
+            if (allChecked == true)
+            {
+                currentTemperature = previousProbableTemperature;
+            }
+
+            return previousProbableTemperature;
+        }
+
+        //Обучаем НЛ
+        void learningAlgorithm(float differenceBetween,
+                               int currentChosenState)
+        {
+            int notUnicalStates = 0;
+
+            //Проверяем, видели ли мы такое состояние ранее
+            for (int i = 0; i < usedState.Length; i++)
+            {
+                if (currentChosenState == usedState[i])
+                {
+                    notUnicalStates++;
+                }
+            }
+
+            //Если данное состояние не уникально
+            if (notUnicalStates >= 1)
+            {
+                //Если разница положительна
+                if (differenceBetween > 0)
+                {
+                    //Если разница больше, чем первое справа значение
+                    if (differenceBetween > NmC2)
+                    {
+                        //Но меньше, чем второе справа значение
+                        if (differenceBetween < NmC1)
+                        {
+                            //Первое справа обновляем
+                            NmC2 = NmC1;
+                            //Второе справа обновляем
+                            NmC1 = differenceBetween;
+                        }
+
+                        //Да еще и больше, чем второе справа значение
+                        else
+                        {
+                            //Первое справа обновляем
+                            NmC2 = differenceBetween;
+                        }
+                    }
+
+                    //Если разница меньше, чем первое справа значение
+                    else
+                    {
+                        //Да еще и меньше, чем второе справа значение
+                        if (differenceBetween < NmC1)
+                        {
+                            //Второе справа обновляем
+                            NmC1 = differenceBetween;
+                        }
+                    }
+                }
+
+                //Если разница отрицательна
+                if (differenceBetween < 0)
+                {
+                    //Если разница меньше, чем первое слева значение
+                    if (differenceBetween < CmN2)
+                    {
+                        //Но больше, чем второе слева значение
+                        if (differenceBetween > CmN1)
+                        {
+                            //Первое слева обновляем
+                            CmN2 = CmN1;
+                            //Второе слева обновляем
+                            CmN1 = differenceBetween;
+                        }
+
+                        //Да еще и меньше, чем второе слева значение
+                        else
+                        {
+                            //Первое слева обновляем
+                            CmN2 = differenceBetween;
+                        }
+                    }
+
+                    //Если разница больше, чем первое слева значение
+                    else
+                    {
+                        //Если разница больше, чем второе слева значение
+                        if (differenceBetween > CmN1)
+                        {
+                            //Второе слева обновляем
+                            CmN1 = differenceBetween;
+                        }
+                    }
+                }
+            }
+
+            //Если уникально
+            else
+            {
+                //Добавляем это состояние в список известных
+                Array.Resize(ref usedState, usedState.Length + 1);
+                usedState[usedState.Length - 1] = currentChosenState;
+
+                //Если разница положительна
+                if (differenceBetween > 0)
+                {
+                    //Если разница больше, чем второе справа значение
+                    if (differenceBetween > NmC1 && NmC1 > 0)
+                    {
+                        //Да еще и больше, чем первое справа значение
+                        if (differenceBetween > NmC2)
+                        {
+                            //Первое справа обновляем
+                            NmC2 = differenceBetween;
+                        }
+                    }
+
+                    //Если разница меньше, чем второе справа значение
+                    else
+                    {
+                        //Но больше, чем первое справа значение
+                        if (differenceBetween > NmC2)
+                        {
+                            //Первое справа обновляем
+                            NmC2 = NmC1;
+                        }
+
+                        //Второе справа обновляем
+                        NmC1 = differenceBetween;
+                    }
+                }
+
+                //Если разница отрицательна
+                if (differenceBetween < 0)
+                {
+                    //Если разница меньше, чем второе слева значение
+                    if (differenceBetween < CmN1 && CmN1 < 0)
+                    {
+                        //Да еще и меньше, чем первое слева значение
+                        if (differenceBetween < CmN2)
+                        {
+                            //Первое слева обновляем
+                            CmN2 = differenceBetween;
+                        }
+                    }
+
+                    //Если разница больше, чем второе слева значение
+                    else
+                    {
+                        //Второе слева обновляем
+                        CmN1 = differenceBetween;
+                    }
+                }
+            }
+        }
+    }
+}
